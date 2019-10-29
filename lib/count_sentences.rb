a0e52c5a7ab3dc051de893600e4abcae9e22d43d
Tariq Bailey <tariqbailey627@gmail.com>
2019-10-29 22:14:33 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+ self.squeeze.split(/[.?!]/)
+counter = 0
+self.split.each do |string|
+  counter += 1 if string.end_with?('.') || string.end_with?('?') || string.end_with?('!')
+end
   end
 end
